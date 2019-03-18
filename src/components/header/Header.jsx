73918907c2dd4629212cc035306b42869108a92d
Alex Leleu <alexleleu@Alexs-MacBook-Pro-2.local>
2019-03-18 17:17:01 +0200

    cleared-previous-version

--- src/components/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-//header is divided in two sections
-import MainComponent from "./MainComponent";
-import MenuComponent from "./MenuComponent";
-
-export default function Header(props) {
-    return <header>{/* our divided header */}</header>;
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./main/app";
-
-import "./assets/less/index.css";
-
-const target = document.getElementById("root");
-
-render(<App />, target);
